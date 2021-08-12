f4796ec2ec7e0ca29e07845a1897084b8ec2992e
Jerico Godoy <jericogodoy7296@gmail.com>
2021-08-13 01:40:14 +0800

    Product landing page finished

---
+++ product-landing-page/index.js
@@ -0,0 +1,24 @@
+const togglebtn = document.getElementsByClassName('toggler')[0]
+const navlinks = document.getElementsByClassName('menu')[0]
+
+navlinks.addEventListener('click',()=>{
+    togglebtn.checked=false;
+})
+
+
+/* CAROUSEL START */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+/* CAROUSEL END */
