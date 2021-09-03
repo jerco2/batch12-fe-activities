5797b08eaf20648b091b48c70075a465cf9a7e26
Jerico Godoy <jericogodoy7296@gmail.com>
2021-09-03 20:31:32 +0800

    updated select player function: added player O

--- projects/tictactoe/script.js
+++ projects/tictactoe/script.js
@@ -3,7 +3,13 @@ const selectPlayerO = document.querySelector("#playerO");
 const board = document.querySelector(".board");
 const selectFirstPlayer = document.querySelector(".selectFirstPlayer");
 
+/* --SELECT WHO'S TURN FIRST
+    BOARD CLASS ADD x or o */
 selectPlayerX.addEventListener("click", () => {
   board.classList.add("x");
+  selectFirstPlayer.classList.add("off");
+});
+selectPlayerO.addEventListener("click", () => {
+  board.classList.add("circle");
   selectFirstPlayer.classList.add("off");
 });
