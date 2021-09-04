2daf6b0ff3ebd1496d13f596fe902348e1885532
Jerico Godoy <jericogodoy7296@gmail.com>
2021-09-04 09:53:01 +0800

    js updated: added placeMark function

--- projects/tictactoe/script.js
+++ projects/tictactoe/script.js
@@ -1,15 +1,44 @@
 const selectPlayerX = document.querySelector("#playerX");
 const selectPlayerO = document.querySelector("#playerO");
-const board = document.querySelector(".board");
 const selectFirstPlayer = document.querySelector(".selectFirstPlayer");
+const board = document.querySelector(".board");
+const cellElements = document.querySelectorAll("[data-cell");
+const xClass = "x";
+const oClass = "circle";
+
+let currentClass = "";
+let circleTurn = "";
+let boardStatus = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 /* --SELECT WHO'S TURN FIRST
     BOARD CLASS ADD x or o */
 selectPlayerX.addEventListener("click", () => {
-  board.classList.add("x");
+  currentClass = xClass;
+  board.classList.add(currentClass);
   selectFirstPlayer.classList.add("off");
 });
 selectPlayerO.addEventListener("click", () => {
-  board.classList.add("circle");
+  currentClass = oClass;
+  board.classList.add(currentClass);
   selectFirstPlayer.classList.add("off");
 });
+
+const handleClick = (e) => {
+  const cell = e.target;
+  /* placemark */
+  placeMark(cell, currentClass);
+  /* check for win */
+  /* check for draw */
+  /* switch turns */
+};
+const placeMark = (cell, currentClass) => {
+  cell.classList.add(currentClass);
+};
+
+for (cell of cellElements) {
+  cell.addEventListener("click", handleClick, { once: true });
+}
