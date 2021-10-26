dc398d7849bea553aa7282d59e445fe9d1b13504
jerco2 <jericogodoy7296@gmail.com>
2021-10-26 16:00:03 +0800

    updated mainpage, added reworked tribute page

---
+++ projectsRework/tributePage/script.js
@@ -0,0 +1,65 @@
+const navbar = document.querySelector(".navbar");
+const image = document.querySelector("#image");
+const movie1 = document.querySelector("#movie1");
+const movie2 = document.querySelector("#movie2");
+const movie3 = document.querySelector("#movie3");
+const body = document.querySelector("body");
+const menu = document.querySelector("#burger");
+const navButtons = document.querySelector(".navButtons");
+
+var lastScrollTop = 0;
+
+// ----- divs slides in on scroll -----
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+  if (image.style.top === 0) {
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "flex";
+  }
+});
+var sliders = document.querySelectorAll(".slide-in");
+const appearOptions = { rootMargin: "0px 0px -200px 0px" };
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+// ----- hides scrollbar after 1.5secs of mousewheel inactivity -----
+document.onwheel = (function () {
+  var onmousestop = function () {
+      body.style.overflowY = "hidden";
+    },
+    thread;
+  return function () {
+    clearTimeout(thread);
+    thread = setTimeout(onmousestop, 1500);
+  };
+})();
+
+// ----- burger menu functions -----
+burger.addEventListener("click", function () {
+  navButtons.classList.add("active");
+});
+navButtons.addEventListener("click", function () {
+  navButtons.classList.remove("active");
+});
