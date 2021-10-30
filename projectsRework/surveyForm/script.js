3ad343c73bfaa7b51cb1151e51d183036e760dd7
jerco2 <jericogodoy7296@gmail.com>
2021-10-30 17:51:16 +0800

    bug fixing update: remove autoselect

--- projectsRework/surveyForm/script.js
+++ projectsRework/surveyForm/script.js
@@ -56,7 +56,6 @@ nameFormNextButton.addEventListener("click", function (e) {
   } else {
     errorStyleOff(nameInput, nameInputError);
     nextFunction(nameForm, emailForm);
-    emailInput.select();
   }
 });
 emailFormNextButton.addEventListener("click", function (e) {
@@ -71,7 +70,6 @@ emailFormNextButton.addEventListener("click", function (e) {
   } else if (valid === true) {
     errorStyleOff(emailInput, emailInputError);
     nextFunction(emailForm, numberForm);
-    numberInput.select();
   } else {
     emailInputError.innerHTML = "Invalid email format";
     errorStyleOn(emailInput, emailInputError);
@@ -92,7 +90,6 @@ surveyIntroForm.addEventListener("click", function (e) {
   e.preventDefault();
   nextFunction(surveyIntroForm, questionForm);
   questionForm.style.display = "flex";
-  questionForm.style.opacity = "1";
 });
 
 // NUMBER INPUT ONLY ACCEPTS NUMBERS
